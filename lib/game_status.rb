741e03367864956c8e8c37da33bae314dcb53f8b
Mary Irwin <maldenirwin@gmail.com>
2016-11-10 20:11:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -15,13 +16,44 @@ win_index_3 = win_combination[2]
 position_1 = board[win_index_1]
 position_2 = board[win_index_2]
 position_3 = board[win_index_3]
-
 if position_1 == "X" && position_2 == "X" && position_3 == "X"
   return win_combination
 elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
   return win_combination
-else
-  return false
     end
   end
+  return false
+end
+
+def full?(board)
+  board.all? {|player| player != " "}
+end
+
+def draw?(board)
+if won?(board) == false && full?(board) == true
+  return true
+elsif won?(board) == false && full?(board) == false
+  return false
+else
+  return false
+  end
+end
+
+def over?(board)
+if won?(board) == true || draw?(board) == true || full?(board) == true
+  return true
+  end
+end
+
+def winner(board)
+if won?(board) != false
+  win_combination = won?(board)
+end
+if won?(board) != false && board[win_combination[0]] == "X"
+  return "X"
+elsif won?(board) != false && board[win_combination[0]] == "O"
+  return "O"
+else
+  return nil
+end
 end
