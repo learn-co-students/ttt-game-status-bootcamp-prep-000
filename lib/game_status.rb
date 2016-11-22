817366d51eb1e61bf3dcaa5ee8de3e4fcce6d407
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-11-22 21:45:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,17 +16,10 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    (position_1 =="X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
