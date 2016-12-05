e39f468431d8f33e11f206477e5de50fb05ed96f
Rory Aptekar <me@roryaptekar.com>
2016-12-05 01:59:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,25 +16,10 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.any? do |win_combination|
-    # Grab each index from the win combination array that composes a win
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_taken?(board, win_index_1)
-      if position_1 == position_2 && position_2 == position_3
-        return win_combination
-      else
-        false
-      end
-    else
-      false
-    end
+  WIN_COMBINATIONS.detect do |combination|
+    board[combination[0]] == board[combination[1]] &&
+    board[combination[1]] == board[combination[2]] &&
+    position_taken?(board, combination[0])
   end
 end
 
@@ -43,22 +28,15 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == false && full?(board)
-    true
-  else
-    false
-  end
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  if won?(board) != false || full?(board) || draw?(board)
-    true
-  end
+  won?(board) || full?(board)
 end
 
 def winner(board)
-  if won?(board) != false
-    win_index_1 = won?(board)[0]
-    return board[win_index_1]
+  if win_combination = won?(board)
+    board[win_combination.first]
   end
 end
