f55bcf93d6b7f1114b9e556e82325f85e93cc18f
Gee Li <geeyung.li@gmail.com>
2016-12-24 06:33:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,20 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =
+[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+      position_taken?(board, win_combo[1])
+  end
+end
