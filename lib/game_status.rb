95cff382c68fcf21dbeb27f71bdd3f277e889761
Gee Li <geeyung.li@gmail.com>
2016-12-25 03:49:54 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,15 +9,43 @@ WIN_COMBINATIONS =
   [0,1,2],
   [3,4,5],
   [6,7,8],
-  [0,4,8],
-  [6,4,2],
   [0,3,6],
   [1,4,7],
-  [2,5,8]
-]
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+  ]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
-      position_taken?(board, win_combo[1])
+    if board[win_combo[0]]==board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && board[win_combo[0]] != " "
+      return win_combo
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |full|
+    if full == "X" || full == "O"
+      true
+    end
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  end
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
   end
 end
