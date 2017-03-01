882eaddd6ccb3ceedc50c117ba5a79391f93f5b0
Luis Rincon <rincon_92@hotmail.com>
2017-03-01 23:02:50 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won(board)
+  WIN_COMBINATIONS.detect do |win_array|
+    board[win_array[0]] == "X"
+  end
+end
+
+
+# if there is an x in 0,1,2
+  # check boards first index position from win_array -> X O " "  -- board[win_array[0]]
+  # see if it is the same as the second index position
+  # see if it is the same as the third index position
+  # check to see if the position is taken
