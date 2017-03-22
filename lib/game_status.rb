6918b9890f522f1f9c541fd1a3005454e30ece67
Slim-Beatnik <totem64@gmail.com>
2017-03-22 00:50:20 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -80,9 +80,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)
-    board[won?(board)[0]]
-  else
-    nil
-  end
+  # if
+    won?(board) ? board[won?(board)[0]] : nil
+  # else
+  #   nil
+  # end
 end
