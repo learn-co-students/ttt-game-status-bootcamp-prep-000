7ce89e8a14b708a68a0925a5b032c57bb54d6da0
Tony Nassif <tonynassif@gmail.com>
2017-05-18 15:38:17 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,25 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #Left column
+  [1,4,7], #Middle column
+  [2,5,8], #Right column
+  [0,4,8], #Left start diagonal
+  [2,4,6]  #Right start diagonal
+]
+
+def won?(board)
+winner = []
+WIN_COMBINATIONS.each do |sub_array|
+  if sub_array.all? { |value| board[value] =="X" }
+       winner = sub_array
+  elsif sub_array.all? { |value| board[value] == "O" }
+    winner = sub_array
+    end
+  end
+  winner
+end
