63487c8e3fb56f82e272fee156b4b417333365f9
Chase Paules <cpaules@gmail.com>
2017-06-13 21:32:07 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,46 @@
-# Helper Method
-def position_taken?(board, index)
+def position_taken?(board, index) #returns true if there is a X or O at index, false if it's empty
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], #bot row
+  [0,4,8], #\ diag
+  [2,4,6],  # / diag
+  [0,3,6], #left col
+  [1,4,7], #mid col
+  [2,5,8] #right col
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]]
+      if board[combination[0]] == "X" || board[combination[0]] == "O"
+        return combination
+      end
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.none?{|index|  index.nil? || index == " "}
+end
+
+def winner(board)
+  won = won?(board)
+  if won != false
+    return board[won[0]]
+  else
+    return nil
+  end
+end
+
+def draw?(board)
+  full?(board) && won?(board) == false
+end
+
+def over?(board)
+  draw?(board) || won?(board) != false 
+end
