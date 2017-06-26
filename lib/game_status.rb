9f4ca148247d7494abf483b095b746fdfc5dac3b
Aaron Samuel <aasamuelj@gmail.com>
2017-06-26 01:26:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,64 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    position_taken?(board, combo[0]) && board[combo[0]] == board[combo[1]] && board[combo[2]] == board[combo[1]]
+  end
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+
+  end
+end
+
+def draw?(board)
+  if full?(board) && !(won?(board))
+    return true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    false
+  end
+end
+
+# def winner(board)
+#   winning_char = won?(board)
+#   if board[winning_char[0]] == "X"
+#     return "X"
+#   elsif board[winning_char[0]] == "O"
+#     return "O"
+#   elsif !(won?(board))
+#     nil
+#   end
+# end
+
+def winner(board)
+  winning_char = won?(board)
+  if !(won?(board))
+    return nil
+  elsif board[winning_char[0]] == "X"
+    return "X"
+  elsif board[winning_char[0]] == "O"
+    return "O"
+  end
+end
