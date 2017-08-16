937f549ccee801846a96e0896fd6ad6e781d51ae
Nick Cianci <n.cianci3@gmail.com>
2017-08-16 01:04:07 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,43 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    next unless position_taken?(board, combo.first)
+    first_token = board[combo.first]
+    return combo if combo.drop(1).all? { |idx| board[idx] == first_token }
+  end
+
+  nil
+end
+
+def full?(board)
+  board.none? { |el| el == " " }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board)
+end
+
+def winner(board)
+  winning_combo = won?(board)
+  return unless winning_combo
+
+  winning_token = board[winning_combo.first]
+  winning_token
+end
