ff1480ca15b1f99e7df3479a7e87693a4f7a0b37
Chris Snyder <christopher.snyder.m+flatiron@gmail.com>
2017-09-09 14:29:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2] # top row
+  [3, 4, 5] # middle row
+  [6, 7, 8] # bottom row
+  [0, 3, 6] # left column
+  [1, 4, 7] # middle column
+  [2, 5, 8] # right column
+  [0, 4, 8] # top-left to bottom-right diagonal
+  [2, 4, 6] # top-right to bottom-left diagonal
+]
