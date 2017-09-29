f5694f4f9ef81ca0a10631f3494083f41b31273f
Anthony Grimes <agrimes@charter.net>
2017-09-29 21:28:13 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,34 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+def won?(board)
+    board.all? { |spot| spot == "" }
+    
+end
+
+def full?(board)
+
+end
+
+def draw?(board)
+
+end
+
+def over?(board)
+
+end
+
+def winner(board)
+
+end
