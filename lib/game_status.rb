0f4d7e6491b5120aa9560a1de23896d1d599dadb
Anthony Grimes <agrimes@charter.net>
2017-09-29 21:59:47 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,8 +16,34 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-    board.all? { |spot| spot == "" }
-    
+    board.all? { |spot|
+        spot == ""
+        break
+    }
+    # define winning indexes
+    win_index_1 = WIN_COMBINATIONS[0]
+    win_index_2 = WIN_COMBINATIONS[1]
+    win_index_3 = WIN_COMBINATIONS[2]
+    win_index_4 = WIN_COMBINATIONS[3]
+    win_index_5 = WIN_COMBINATIONS[4]
+    win_index_6 = WIN_COMBINATIONS[5]
+    win_index_7 = WIN_COMBINATIONS[6]
+    win_index_8 = WIN_COMBINATIONS[7]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    position_4 = board[win_index_4]
+    position_5 = board[win_index_5]
+    position_6 = board[win_index_6]
+    position_7 = board[win_index_7]
+    position_8 = board[win_index_8]
+
+    if position_1 == "X"
+        return WIN_COMBINATIONS
+    else
+        false
+    end
 end
 
 def full?(board)
