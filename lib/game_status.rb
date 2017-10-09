b0e0754717585e835a4af68daea14750f00e97ac
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-09 22:50:30 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,51 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #first row
+  [3,4,5], #second row
+  [6,7,8], #third row
+  [0,3,6], #first column
+  [1,4,7], #second column
+  [2,5,8], #third column
+  [0,4,8], #Right diagonal
+  [2,4,6] #Left diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if (position_1 == "X" && position_2 == "X" && position_3 =="X") || (position_1 == "O" && position_2 == "O" && position_3 =="O")
+      return win_combination
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|space|
+    space == "X" || space == "O"
+  }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
+  end
+end
