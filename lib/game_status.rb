068a3f807cc3dac8d0944c988330a04c32b26e92
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-16 21:10:07 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,15 +14,38 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
-def won?(board)
-  result = []
-WIN_COMBINATIONS.each do |win_combination|
-win_combination.all? do |i|
-  board[i]=="X" || board[i]=="O"
-  result << win_combination[i]
-end
-end
-else
-  false
 
+def won?(board)
+  # you can't check won?(board) == false
+  # you can check it with !won?(board) or won?(board) == nil
+WIN_COMBINATIONS.find do |win_combination|
+  win_combination.all? {|i| board[i]=="X"} ||
+  win_combination.all? {|i| board[i]=="O"}
+end
+end
+
+def full?(board)
+  board.all? do |entry|
+    entry=="X" || entry=="O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+    won?(board).each do |i|
+      if board[i]=="X"
+        return "X"
+      else
+        return "O"
+     end
+   end
+ end
 end
