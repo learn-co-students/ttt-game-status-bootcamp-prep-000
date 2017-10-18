35886d55aa9a898ad22b85c18f177e0f11db2b26
Simcha Wood <truthfulthomas@gmail.com>
2017-10-18 01:37:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,64 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    x, y, z = combo
+
+    if [board[x], board[y], board [z]].all? {|i| i == "X"}
+      return combo
+    elsif [board[x], board[y], board [z]].all? {|i| i == "O"}
+      return combo
+    end
+  end
+  return nil
+end
+
+def full?(board)
+  board.none? {|i| i == " "}
+end
+
+def draw?(board)
+  if won?(board)
+    return false
+  elsif full?(board)
+    return true
+  end
+  return false
+end
+
+def over?(board)
+  if won?(board)
+    return true
+  elsif draw?(board)
+    return true
+  end
+  return false
+end
+
+def winner(board)
+  winner = nil
+  if won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      x, y, z = combo
+
+      if [board[x], board[y], board [z]].all? {|i| i == "X"}
+        winner = "X"
+      elsif [board[x], board[y], board [z]].all? {|i| i == "O"}
+        winner = "O"
+      end
+    end
+  end
+  winner
+end
