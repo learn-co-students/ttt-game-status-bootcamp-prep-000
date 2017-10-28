1f8676a56a868e1a92b9b6157d50f55204a8d39c
Doug Harman <dharman@4harman.com>
2017-10-28 01:26:32 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,62 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+def won?(board)
+  board.each do |index|
+    if (index == nil? || index == " ")
+      return false
+    end
+  end
+  board.each do |index|
+    if !(index == nil? || index == " ") && !(board.include?("'X','X','X'") || board.include?("'O','O','O'"))
+      return false
+    end
+  end
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
+  end
+end
+
+def full?(board)
+  board.any do |index|
+    if draw?(board) == true
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def draw?(board)
+  board.each do |index|
+    if !(index == nil? || index == " ") && !(board.include?("'X','X','X'") || board.include?("'O','O','O'"))
+      return true
+    elsif won?(board) == true
+      return false
+    else
+      return false
+    end
+  end
+end
