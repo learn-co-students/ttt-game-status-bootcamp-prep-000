b056ded28579f0d24b925a8bde51fc0ae7c17a41
Doug Harman <dharman@4harman.com>
2017-10-30 01:42:44 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,6 +2,8 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+# If position not taken, !" " == false and " " == true
+# If position taken,     !" " == true  and " " == false
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
@@ -32,25 +34,41 @@ def won?(board)
 end
 
 def full?(board)
-  if draw?(board) == true
-    return true
+  if board.all? { |index| !(index.nil? || index == " ") }
+    true
   else
-    return false
+    false
   end
 end
 
 def draw?(board)
-  board.each do |index|
-    if !(index == nil? || index == " ") && !(board.include?("'X','X','X'") || board.include?("'O','O','O'"))
-      return true
+  if won?(board) == false && full?(board) == true
+    return true
+  elsif won?(board) == false && full?(board) == false
+    return false
+  elsif won?(board) == true
+    return false
+  end
+end
+
+def over?(board)
+  if draw?(board) == true
+    true
+  elsif won?(board) !=false
+    true
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |position|
+      if (position[1] == "X" && position[2] == "X" && position[3] == "X")
+        return "X"
+      elsif (position[1] == "O" && position[2] == "O" && position[3] == "O")
+        return "O"
+      else
+        return nil
+      end
     end
   end
-  false
-end
-
-def over(board)
-
-end
-
-def winner
 end
