c7d28111b73f5f321109d12861bf8f1320f51c7d
PJ Frias <peejfrias@gmail.com>
2017-11-21 12:33:11 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,41 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
+def won?(board)
+
+end
+
+
+def full?(board)
+
+end
+
+
+def draw?(board)
+
+end
+
+
+def over?(board)
+
+end
+
+
+def winner?(board)
+
+end
+
+
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left column
+  [1,4,7],  # Middle column
+  [2,5,8],  # Right column
+  [0,4,8],  # Forward diagonal
+  [2,4,6]  # Backward diagonal
+]
