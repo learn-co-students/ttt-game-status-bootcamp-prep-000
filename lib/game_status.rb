a353c27e55032c97dfa8ee55e912d3af951d792d
Christine Chow <christine@cadenzaglobal.com>
2018-01-07 05:06:18 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,3 +15,29 @@ WIN_COMBINATIONS = [
     [0,4,8],
     [6,4,2]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
