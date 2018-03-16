2e4b97fdd4b7c71a7a7492505282cc373d16cd25
nancy sin <nsin@sbcglobal.net>
2018-03-16 21:17:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,28 @@ WIN_COMBINATIONS = [
 [0,4,8],
 [2,4,6],
 ]
+def won?(board)
+  WIN_COMBINATIONS.detect do |combination|
+    board[combination[0]] == board[combination[1]] &&
+    board[combination[1]] == board[combination[2]] &&
+    position_taken?(board, combination[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+ full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+ if winning_combination = won?(board)
+   board[winning_combination.first]
+  end
+end 
