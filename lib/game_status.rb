70bf30e606b2934c970a19c80f781bc5ff726309
jconway4 <jconwa4@illinois.edu>
2018-03-26 17:15:44 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,56 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    puts win_combination
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+      return [win_index_1, win_index_2, win_index_3]
+    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return [win_index_1, win_index_2, win_index_3]
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
   end
 end
+
+def draw?(board)
+  if won?(board)
+    return false
+  elsif full?(board)
+    return true
+  else
+    return false
+  end
+end
+  
+def over?(board)
+  if won?(board)
+    return true
+  elsif draw?(board)
+    return true
+  else
+    return false
+  end
+end
+  
+def winner(board)
+  if won?(board)
+    getWinner = won?(board)
+    getWinnerIndex = getWinner[0]
+    return board[getWinnerIndex]
+  else
+    return nil
+  end
+end
+  
