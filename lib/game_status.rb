15f1843a0600aa4e196d28eb088afcfbd28aeaa5
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-04 04:04:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,60 @@
+require "pry"
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+    return combo
+  elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    return combo
+  end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|space| space == "X" || space == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+  true
+else
+  false
+end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    true
+else
+  false
+end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+    return "X"
+  elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    return "O"
+  end
+  end
+  return nil
+end
