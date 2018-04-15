1a55db66ec927c701fb492ba79d4b94e7ccf6341
github username <github email address>
2018-04-15 13:34:07 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,79 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6],
+    [2,5,8]
+]
+
+def won? (board)
+    WIN_COMBINATIONS.each do |combination|
+        win_index_1 = combination[0]
+        win_index_2 = combination[1]
+        win_index_3 = combination[2]
+        
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+        
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+            return combination
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+            return combination
+        else
+            false
+            end
+        end
+    false
+end
+
+def full? (board)
+    board.all? {|element| element == "O" || element == "X"}
+end
+
+def draw? (board)
+    if won?(board) == false && full?(board) == true
+        return true
+    else
+        false
+    end
+end
+
+def over?(board)
+    if won?(board) == true || draw?(board) == true || full?(board) == true
+        return true
+    elsif won?(board) == false && draw?(board) == false && full?(board) == false
+        false
+    else
+        true
+    end
+end
+
+def winner (board)
+  if won?(board) != false
+      #binding.pry
+      letter = won?(board)
+        if board[letter[0]] == "X"
+            return "X"
+        elsif board[letter[0]] == "O"
+            return "O"
+        else
+            return "nil"
+      #binding.pry
+    end
+  end
+end
+
+
+
+
+
+
+
+
