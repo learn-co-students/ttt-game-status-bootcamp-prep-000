14876f890d9322de8d2b96ff6ea76ec3412ab75e
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-27 17:13:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,50 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def winning_combos(board)
+  WIN_COMBINATIONS.select do |winning_combo|
+    (board[winning_combo[0]] == "X" && board[winning_combo[1]] == "X" && board[winning_combo[2]] == "X") || (board[winning_combo[0]] == "O" && board[winning_combo[1]] == "O" && board[winning_combo[2]] == "O")
+  end
+end
+
+def won?(board)
+  winning_combos(board).first
+end
+
+def full?(board)
+  !board.any? do |board_position|
+   board_position == " "
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  winning_combination = won?(board)
+  if winning_combination == nil
+    return nil
+    elsif board[winning_combination[0]] == "X"
+    return "X"
+  else
+    "O"
+  end
+end
+    
+    
