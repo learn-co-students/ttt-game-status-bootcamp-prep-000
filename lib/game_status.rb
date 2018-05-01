2c4cccfc6541ab8f1c891b6977c2d8f8f6f356b8
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-05-01 20:11:01 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,5 +15,52 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
   ]
 def won?(board)
-  
+  WIN_COMBINATIONS.each do |combo|
+    if ((board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O") )
+      return combo
+    end
+  end
+  return false;
+end
+
+def full? (board)
+  index = 0
+  board.each do |cell|
+    if !position_taken?(board, index)
+      return false;
+    end
+    index += 1
+  end
+  return true
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board)
+    return true
+  elsif won?(board) == false && !full?(board)
+    return false
+  elsif won?(board) != false
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) != false || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if !draw?(board) && over?(board)
+    comboArray = won?(board)
+    if board[comboArray[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  else
+    return nil
+  end
 end
