587ab951b881cf30361a1fbc785e82b22a3d716c
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-05-04 00:50:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    position_1 = board[win_combination[0]]
+    position_2 = board[win_combination[1]]
+    position_3 = board[win_combination[2]]
+    if position_1 == position_2 && position_1 == position_3 && (position_1 == "O" || position_1 == "X")
+      return win_combination
+    else 
+      return false 
+    end 
+  end
+end
