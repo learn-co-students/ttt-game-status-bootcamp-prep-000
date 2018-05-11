00961cd362301decd67f98dc60248f0c71f563d5
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-11 13:37:41 -0400

    won? potentially done

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -17,7 +15,28 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  
+
+=begin
+1. loop through win WIN_COMBINATIONS
+
+2. match the win combination values with board indexes
+
+3. compare those values to see if they're all X or O's
+
+4. if so return the matching WIN_COMBINATION
+=end
+
+result = false
+
+WIN_COMBINATIONS.each do |set|
+  if board[set[0]] == "X" && board[set[1]] == "X" && board[set[2]] == "X"
+    result = set
+  elsif board[set[0]] == "O" && board[set[1]] == "O" && board[set[2]] == "O"
+    result = set
+  end
+end
+
+result
 end
 
 def full?(board)
@@ -31,4 +50,3 @@ end
 
 def winner(board)
 end
-
