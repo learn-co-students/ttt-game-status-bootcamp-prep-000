b961437407c6114bdde77f9d3ce62426a1170751
jmoypace <jmoypace@gmail.com>
2018-05-24 17:13:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,50 @@
+
+ require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def won?(board)
+WIN_COMBINATIONS.detect do |win|
+  # binding.pry
+   board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]] && position_taken?(board, win[0])
+ end
+end
+
+
+def full?(board)
+  board.all? do|exist|
+  exist=="X"||exist=="O"
+    
+  end
+  
+end
+
+def draw?(board)
+!won?(board) && full?(board)
+end
+
+def over?(board)
+
+  won?(board) || full?(board)
+
+end
+
+def winner?(board)
+  won?(board).include("X")
+  return "X"
+
+end
