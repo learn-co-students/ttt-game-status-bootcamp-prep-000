a66f2c8ed5a520c4a0a92302fffbdec4dff37e6b
Katy <komal.vadukul@gmail.com>
2018-06-05 16:08:16 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,35 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+  WIN_COMBINATIONS = [[0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [6,4,2]]
+def won?(board)
+  WIN_COMBINATIONS.detect do|win_index|
+        (board[win_index[0]] == board[win_index[1]]) && (board[win_index[1]] == board[win_index[2]]) && position_taken?(board, win_index[0])
+  end
+end
+
+def full?(board)
+  board.all? do|index|
+     (index != " ")
+  end
+end
+
+def draw?(board)
+   full?(board) && !won?(board)
+end
+
+def over?(board)
+   won?(board) || full?(board)
+end
+
+def winner(board)
+  won?(board) && !draw?(board)
+    "X"
+end
