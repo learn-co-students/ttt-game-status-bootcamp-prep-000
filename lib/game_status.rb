9d1c1e61f6c8985feec46aa616aee74e7ba511d0
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-23 22:53:03 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,6 +6,53 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+
 def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+
+if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    return win_combination 
+  else
+    false
+  end
+end
+
+
+# if board.not_include? "X" || "O"
+
+
+# For an ARRAY consisting of 8 3-figure sub-arrays
+# IF the ARRAY does .INCLUDE? a SUB_ARRAY == ["X', 'X', 'X'"] || ["O', 'O', 'O'"]
+# THEN 
+# WIN_COMBINATIONS[Z]
 
 end
+
+
+def full?(board)
+  board.all? do |board_element| 
+    board_element == "X" || board_element == "O"
+  end
+end
+
+
+def draw?(board)
+
+end
+
+
+def over?(board)
+  
+end
+
+
+def winner(board)
+  
+end 
