3b672f137e4d3c5a7f2a06585ca152ea36ae462d
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-24 19:13:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,7 @@ def won?(board)
     position_3 = board[win_index_3]
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination 
-    else 
-      false
+    
     end
   end 
 end
