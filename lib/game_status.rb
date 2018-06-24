d687a02afac71c92e76d309482dd1f98de45a821
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-24 19:44:35 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
@@ -26,8 +27,18 @@ def won?(board)
   end
 end
 
+
 def full?(board)
   board.all? do |board_element| 
     board_element == "X" || board_element == "O"
   end
+end
+
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else 
+    false 
+  end 
 end
