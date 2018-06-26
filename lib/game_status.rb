c7f45dd1cd24a8e7fb6c40229d489a8c371b76a8
Steven Crouse <sdcrouse91@gmail.com>
2018-06-26 09:59:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,14 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal 1 (negative slope)
+  [2,4,6]  # Diagonal 2 (positive slope)
+]
+
