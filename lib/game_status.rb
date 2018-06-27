a6a52fbf0ddbc19db9880d7f610564cbd36031f1
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-27 09:43:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,51 +6,51 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
-
 def won?(board)
 
 win_combo_length = WIN_COMBINATIONS.length - 1
 	for a in 0..win_combo_length
 	win_combo= WIN_COMBINATIONS[a]
 	check_board = []
-	
 		for i in 0..2
 		check_board.push(board[win_combo[i]])
 		i += 1
 		end
-			if check_board.all? { |elements| elements == "X" || elements == "O"}
+			if check_board.all? { |elements| elements == "X"}
 				return win_combo
-			else
-		  a += 1
-		end	
+			elsif check_board.all? { |elements| elements == "O"}
+				return win_combo 
+			end
+			a += 1
 	end
-	if board.all? { |markers| markers != ""}
-	  return false
-	elsif full?(board) == true
+	if board.any? { |elements| elements != " "}
+  else
     return false
 	end
 end
 
-
 def full?(board)
-    if board.any? { |markers| markers != ""}
-    return false
-  elsif draw?(board) == true
+    if board.any? { |markers| markers == " "}
+      return false
+    else 
+      return true
   end
 end
 
 
 def draw?(board)
-  won?(board) == false && full?(board) == true
+  if won?(board).kind_of?(Array) == false && full?(board) == true
+    return true
+  else
+    return false
+  end
 end
 
 
 def over?(board)
   if draw?(board) == true
     return true
-  elsif won?(board) ==true && full?(board) == true
-  return true
-  elsif won?(board) ==true && full?(board) == false
+  elsif won?(board).kind_of?(Array) == true
   return true
   elsif full?(board) == false 
     return false
@@ -59,5 +59,12 @@ end
 
 
 def winner(board)
-
+  won_combo = won?(board)   
+  if won?(board) == nil
+    return nil
+  elsif board[won_combo[0]] == "X"
+    return "X"
+  elsif board[won_combo[0]] == "O"
+    return "O"
+  end
 end
