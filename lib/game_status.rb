3c6843baca03275cdfa36263bb712dcaf57a20f7
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-19 22:01:19 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,19 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+def won()
+  
+end
+
+def win_condition()
+  win_combinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6]
+]
+end
