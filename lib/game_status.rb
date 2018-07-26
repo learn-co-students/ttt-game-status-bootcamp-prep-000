6e8060e69ea5e49e1839c0f9bdc063b4715c7d21
Will Carter <willmckenna64@gmail.com>
2018-07-26 19:00:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -16,8 +15,28 @@ WIN_COMBINATIONS = [
   ]
   
   def won?(board)
-    WIN_COMBINATIONS.each do |win_array|
-      win_array.each do |sqaures|
-        if position_taken?(board, 0) == false || position_taken?(board, 1) == false || position_taken?(board, 2) == false
-          
+    win_index_1 = WIN_COMBINATIONS[0]
+    win_index_2 = WIN_COMBINATIONS[1]
+    win_index_3 = WIN_COMBINATIONS[2]
+    win_index_4 = WIN_COMBINATIONS[3]
+    win_index_5 = WIN_COMBINATIONS[4]
+    win_index_6 = WIN_COMBINATIONS[5]
+    win_index_7 = WIN_COMBINATIONS[6]
+    win_index_8 = WIN_COMBINATIONS[7]
+    win_index_9 = WIN_COMBINATIONS[8]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    position_4 = board[win_index_4]
+    position_5 = board[win_index_5]
+    position_6 = board[win_index_6]
+    position_7 = board[win_index_7]
+    position_8 = board[win_index_8]
+    position_9 = board[win_index_9]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+      else false
+    end
   end
