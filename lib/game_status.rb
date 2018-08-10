6702f99658a8c92c38b4ffafd9cac7ac4b5212ff
Hilary Sturges <hilarysturges@gmail.com>
2018-08-10 21:26:34 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,44 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+
+
+def full?(board)
+  if board.include?(" " || "" || nil)
+    return false
+  else 
+    return true 
+  end
+end
+
+def draw?(board)
+  if won?(board)
+    return false
+  elsif full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if (won?(board) || full?(board) || draw?(board))
+    return true
+  else
+    return false
+  end
+end 
+
+
+  
