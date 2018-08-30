f6d232aee4af26afbec4a1b6362d595f4f217ab4
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-30 17:09:08 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,27 +5,68 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
 
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Across
-  [3,4,5],
-  [6,7,8],
-  [0,3,6], # Down
-  [1,4,7],
-  [2,5,8],
-  [0,4,8], # Diagonal
-  [2,4,6] 
-  ]
-  
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
 def won?(board)
-  WIN_COMBINATIONS.each do |i|
-  win_index_1 = i[0]
-  win_index_2 = i[1] 
-  win_index_3 = i[2]
-   if ((board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X") || (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"))
-    return [i[0], i[1], i[2]]
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
     end
   end
-  puts false
-end  
+end
