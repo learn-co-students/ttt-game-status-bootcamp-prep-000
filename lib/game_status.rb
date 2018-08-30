911bf2c5a16273dffb7f4b5f65e72179590366b4
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-30 14:41:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,31 @@
+ #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Across
+  [3,4,5],
+  [6,7,8],
+  [0,3,6], # Down
+  [1,4,7],
+  [2,5,8],
+  [0,4,8], # Diagonal
+  [2,4,6] 
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |i|
+  win_index_1 = i[0]
+  win_index_2 = i[1] 
+  win_index_3 = i[2]
+   if ((board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X") || (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"))
+   return "#{i}"
+    end
+  end
+  puts false
+end  
