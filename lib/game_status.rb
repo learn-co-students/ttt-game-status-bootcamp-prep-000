f6acab83376dc4326d62fc82c7858e3a5d9cffca
Olivia Kasten <odkasten@gmail.com>
2018-09-04 19:18:40 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,68 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+    end 
+  end
+  return false
+end 
+
+def full?(board)
+  if board.include?(" ")
+    false
+  else 
+    true 
+  end 
+end 
+
+def draw?(board)
+  if full?(board) && !won?(board)
+      true
+  else
+    false
+  end 
+end 
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  index = won?(board)
+  if over?(board) && won?(board)
+    if board[index[0]].include?("X")
+      "X"
+    elsif board[index[0]].include?("O")
+      "O"
+    end 
+  else
+    nil
+  end 
+end 
+    
