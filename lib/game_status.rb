ee5f0abc0bae9a8e394496a12b4888eca9f4e1a0
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-09 17:06:48 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,36 @@ WIN_COMBINATIONS = [
   [1, 4, 7], 
   [2, 5, 8]
   ]
+  
+  def won?(board)
+    
+    WIN_COMBINATIONS.each do |pos|
+      return pos if board[pos[0]] == "X" && board[pos[1]] == "X" && board[pos[2]] == "X" 
+      return pos if board[pos[0]] == "O" && board[pos[1]] == "O" && board[pos[2]] == "O"
+    end 
+   return nil if full?(board)
+  end 
+  
+  def full?(board)
+    board.all?{ |el| el != " "}
+  end 
+  
+  def draw?(board)
+   return false if full?(board) && won?(board) 
+   return false unless full?(board)
+   true
+  end 
+  
+  def over?(board)
+    return true if draw?(board)
+    return true if won?(board)
+  end 
+  
+  def winner(board)
+    if won?(board)
+     pos = won?(board)
+     return board[pos[0]]
+    else 
+     return nil
+    end
+  end 
