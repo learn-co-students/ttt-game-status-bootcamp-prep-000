6883d64a0a3dd0b0a22e07522904e5471530604c
Ava Barron <mztriz@gmail.com>
2018-10-24 21:45:22 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,6 +7,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] and board[combo[1]] == board[combo[2]] and position_taken?(board, combo[0])
+    #placeholder
   end
 end
 
