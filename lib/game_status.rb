ff816285c4d638e02442ef1d81f675055c2d3c0e
Mary Selig <seligmar@gmail.com>
2018-11-26 17:11:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,29 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# def input_to_index(user_input)
+#   user_input = user_input.to_i-1
+#   # puts user_input
+# end 
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6], 
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  def won?(board)
+    WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]]== board[combo[1]] && board[combo[1]] == board[combo[2]]
+      puts "#{board[combo]}"
+    else 
+      return false
+end
+end
+end
