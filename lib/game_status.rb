ed1976aec3bfb48681d3f8311f55b92fcccfd25a
Ava Barron <mztriz@gmail.com>
2018-12-19 01:52:39 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,12 +2,33 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] and board[combo[1]] == board[combo[2]] and position_taken?(board, combo[0])
-    #test
+    board[combo[0]] == board[combo[1]] \
+    and board[combo[1]] == board[combo[2]] \
+    and position_taken?(board, combo[0])
   end
 end
 
+def full?(board)
+  for place in board
+    if place.match("") or place.match(" ")
+      return false
+    end
+  end
+  true
+end
+
+def draw?(board)
+end
