beb77305a1e18bf2ccdf282ce6285cc5a38246cc
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-04 16:48:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,34 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS= [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |i|
+    if i.all? { |index| board[index] == "X"} || i.all? { |index| board[index] == "O"}
+      return i
+    end
+  end
+  return nil
+end
+
+def full?(board)
+  board.none?{|spot| spot =="" || spot == " "}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
