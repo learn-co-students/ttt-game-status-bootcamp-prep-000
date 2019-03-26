2624f9a8cffb67b39f318a4aac195cc9d5cdcbcc
Joel S <joel29dec@gmail.com>
2019-03-26 16:29:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,9 +1,9 @@
 
 
 # Define your WIN_COMBINATIONS constant
-#board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+board = ["X", "X", "X", "O", "X", "X", "O", "X", "O"]
 WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
-
+$win = nil
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -25,10 +25,11 @@ winner = false
     # draw = (position_1 == "X" && position_2 == "O" && position_3 == "X") || (position_1 == "O" && position_2 == "X" && position_3 == "O")
 
     if winning_move
-      won = true
       winner = true
+      $win = position_1
       return win_combination # return the win_combination indexes that won.
     else
+      $win = nil
       winner = false
     end
   end
@@ -50,31 +51,16 @@ def full?(board)
 end
 
 def draw?(board)
-    won = won?(board).is_a? Array   
-    if (full?(board) == true || full?(board) == false) && won == true
-    false
-    elsif full?(board) == true && won == false
-     true
-    end 
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  won = won?(board).is_a? Array
-  if draw?(board) == true
-    true
-  else
-    false
-  end
-  
-  
-  end
+  draw?(board) || won?(board) || full?(board)
 end
-    
-  
 
 
+def winner(board)
+  won?(board)
+  $win
+end
 
-
-
-
-  
