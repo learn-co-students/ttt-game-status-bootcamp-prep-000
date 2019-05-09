781aa3a61bb96db5f5aff9e23c2ed54e53206127
Beto Dute <betodute@gmail.com>
2019-05-09 21:28:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-
 top_row_win = [0,1,2]
 midde_row_win = [3,4,5]
 bottom_row_win = [6,7,8]
@@ -14,44 +12,69 @@ third_clm_win = [2,5,8]
 first_diag_win = [0,4,8]
 second_diag_win = [2,4,6]
 
-WIN_COMBINATIONS =
-[
+
+WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
-  [6,7,8]
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-if    board[top_row_win[0]] == "X" &&
-      board[top_row_win[1]] == "X" && 
-      board[top_row_win[2]] == "X"
-      "X won in the top row"
-elsif board[middle_row_win[0]] == "X" &&
-      board[middle_row_win[1]] == "X" && 
-      board[middle_row_win[2]] == "X"
-      "X won in the middle row"
-elsif board[bottom_row_win[0]] == "X" &&
-      board[bottom_row_win[1]] == "X" && 
-      board[bottom_row_win[2]] == "X"
-      "X won in the bottom row"
-elsif board[first_clm_win[0]] == "X" &&
-      board[first_clm_win[1]] == "X" && 
-      board[first_clm_win[2]] == "X"
-      "X won in the first column"
-elsif board[second_clm_win[0]] == "X" &&
-      board[second_clm_win[1]] == "X" && 
-      board[second_clm_win[2]] == "X"
-      "X won in the second column"
-elsif board[third_clm_win[0]] == "X" &&
-      board[third_clm_win[1]] == "X" && 
-      board[third_clm_win[2]] == "X"
-      "X won in the third column"
-elsif board[first_diag_win[0]] == "X" &&
-      board[first_diag_win[1]] == "X" && 
-      board[first_diag_win[2]] == "X"
-      "X won diagonally"
-elsif board[second_diag_win[0]] == "X" &&
-      board[second_diag_win[1]] == "X" && 
-      board[second_diag_win[2]] == "X"
-      "X won diagonally"
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
+
+def full?(board)
+board.all?{|check| check == "X" || check == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+
