ca027baf4ac8c2f67e7f9178cf061fb0b2238386
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-23 22:53:54 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-  i = 0 
   
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
@@ -35,12 +34,44 @@ def won?(board)
 end 
 
 def full?(board)
-  new_board = board.reject{|empty_space| empty_space == " "}
-  if new_board.length == 9
+  x_or_o = board.reject{|empty_space| empty_space == " "}
+  if x_or_o.length == 9
     return true
   else
     false
   end 
 end 
 
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else 
+    false
+  end 
+end 
 
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  end 
+end 
+
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return "X"
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return "O"
+  end
+  end
+  nil
+end 
