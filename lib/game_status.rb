6dfd058a4bc244c1cf7c0070c97804a573b99305
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-24 18:44:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,61 +17,26 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-  
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
-  
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-  
-  if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O" 
-  return win_combination 
+ 
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2] 
+  position_3 = board[win_index_3] 
+ 
+  if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    return win_combination 
   end
-  end
-  return false
-end 
+end
+false
+end
 
 def full?(board)
-  x_or_o = board.reject{|empty_space| empty_space == " "}
-  if x_or_o.length == 9
+  filtered_board = board.reject{|empty_space| empty_space == " "}
+  if filtered_board == board.length 
     return true
-  else
-    false
-  end 
-end 
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    true
   else 
     false
   end 
-end 
-
-def over?(board)
-  if won?(board) || draw?(board) || full?(board)
-    true
-  end 
-end 
-
-def winner(board)
-  WIN_COMBINATIONS.each do |win_combination|
-  
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-  
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-  
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return "X"
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return "O"
-  end
-  end
-  nil
 end 
