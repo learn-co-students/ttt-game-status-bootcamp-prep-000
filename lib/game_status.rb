17ca22e208c85083f18c711e11ab1a1575639a88
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-25 18:09:28 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,9 +34,40 @@ end
 
 def full?(board)
   filtered_board = board.reject{|empty_space| empty_space == " "}
-  if filtered_board == board.length 
+  if filtered_board.length == 9 
     return true
   else 
     false
   end 
+end 
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  end 
+end 
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+ 
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2] 
+  position_3 = board[win_index_3] 
+ 
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return "X" 
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return "O"
+  end
+end
+  nil
 end 
