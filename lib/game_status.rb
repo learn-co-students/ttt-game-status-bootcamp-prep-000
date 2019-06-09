72b4340b4605ae02d82f6e998b24a2bde7312453
John Jordan Shelley <jshelley553@gmail.com>
2019-06-09 01:41:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,34 +3,68 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def win?(board)
 
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [6,4,2]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ]
 
-  WIN_COMBINATIONS.each do |w|
-    index_1 = w[0]
-    index_2 = w[1]
-    index_3 = w[2]
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    index_0 = win[0]
+    index_1 = win[1]
+    index_2 = win[2]
 
-        position_1 = board[index_1]
-        position_2 = board[index_2]
-        position_3 = board[index_3]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return w
-  else
-    position_1 == "0" && position_2 == "O" && position_3 == "O"
-    return w
-      end
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win
     end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
     return false
   end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
