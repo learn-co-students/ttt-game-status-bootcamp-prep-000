e0e56ec94e82c931e263dde23fa6829f8319eb4c
Mark Kauffman <markkauffman22@gmail.com>
2019-10-13 04:20:30 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,5 @@
-# Helper Method
+# THIS PASSES ALL THE TESTS ...
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -12,100 +13,76 @@ WIN_COMBINATIONS = [
   [1,4,7],  # Middle column
   [2,5,8],  # Right column
   [0,4,8],  # Diaonal 1
-  [6,4,2]  # Diagonal 2
+  [2,4,6]  # Diagonal 2
 ]
 
 def empty?(board)
-    board.all? { |pos| (pos.nil? || pos == " " )} # if this is always true, the spot is empty, return true
-end
-
-def full?(board)
-  # returns true for a FULL board (a draw)
-  # the all? method returns true if the block never returns false or nil for any element passed to it:
-  
-  empty_spot = board.any? { |pos| (pos.nil? || pos == " " )}  # if either of these are true, the spot is empty
-  if empty_spot
-    return false
-  else 
-    return true
-  end  
+    board.all? { |pos| (pos.nil? || pos == " " || pos == ""  )} # if these are always true, the board is empty, return true
 end
 
 def won?(board)
-  # return false/nil if there is no win combination or draw
-  # return the winning combination indexes as an array if there is a win.
+  winner = false
+  pos_winners = []
+  return false if empty?(board)  # Nice Short-Cut!!
 
-
-  # The all? method returns true if the block never returns false or nil for any element passed to it:
-  # first indwill be 
-  
   pos_winners = []
   WIN_COMBINATIONS.each do |win_combo|  
       all_3_taken = false
 
       all_3_taken = win_combo.all? do |index|   
-        position_taken?(board, index)       # Will evaluate to true if all indexes are occupied.
+        position_taken?(board, index)       # true if all indexes are occupied.
       end
       if all_3_taken 
-        puts "all_3 ...", win_combo
         pos_winners.push(win_combo)
       end
   end  # WIN_COMBINATIONS.each loop end  
   
   # Now have an array with the possible winners ...  
-  puts "HOW MANY? " 
-  puts pos_winners.length
-  print pos_winners
-  puts "\n?????????????????????"
-  
-  pos_winners.each do |pos_combo| 
-    puts "Possible Winner evaluating ..."
-    print pos_combo
-    x_wins = pos_combo.all? { |i| board[i] == "X"}
-      print "X Wins : "
-      puts x_wins
-      puts "--------------------------------"
-      if x_wins
-        return pos_combo
-      end  
-      
-    o_wins = pos_combo.all? { |i| board[i] == "O"}
-      print "O Wins : "
-      puts o_wins  
-      if o_wins
-        return pos_combo
-      end  
-    
-  end # pos_winners each loop  
+    if pos_winners.length == 0   # no winner, BAIL
+      puts "NO WINNER!"
+      return pos_winners[98]  # returning nil, duh. 
+    end
+
+  # Now see if any pos_winner is "ALL THE SAME" and return it.  
+  winning_combo = false
+  pos_winners.each do |pos_combo|
+    if 
+      (board[pos_combo[0]] == board[pos_combo[1]]) && (board[pos_combo[0]] == board[pos_combo[2]])
+      winning_combo = pos_combo
+    end  
+  end # pos_winners each loop
+  return winning_combo
 end  # this is the won? function end
 
 def full?(board)
-  # returns true for a FULL board (a draw)
-  # the all? method returns true if the block never returns false or nil for any element passed to it:
-  
-  board.each do |this_pos|
-    if (this_pos.nil? || this_pos == " " ) # if either of these are true, the spot is empty, return false.
-      return false
-    end
-    return true
-  end
+  board.none? {|this_pos| (this_pos.nil? || this_pos == " " || this_pos == "")} 
 end
 
 def draw?(board)
-  if full?(board)  #returns true for a draw
-    return true
-  end  
+  return true if !(won?(board)) && full?(board)
+  return false if !(won?(board)) && !(full?(board))
+  return false if won?(board)
 end
 
 def over?(board)
-  if full?(board) ||  won?(board)  #returns true for a WIN OR Draw
+  if full?(board) ||  won?(board)  #returns true for a WIN OR Dra
     return true
   end
   return false
 end
 
+def winner(board)
+  if won?(board)
+    win_combo = won?(board)
+    winner = board[win_combo[0]]   #The first value will be "X" or "O" - the WINNER!
+    puts "WINNER from #winner IS: "
+    winner
+    return winner
+  end
+end  
+
 #          0    1    2    3    4    5    6    7    8
-#board = ["X", "X", "X", "O", "O", " ", " ", " ", " "] #top row
- board = ["X", " ", "O", " ", "X", "O", " ", " ", "X"] #left diagonal
-puts "START"
-won?(board)
+#board = ["X", " ", "O", " ", "X", "O", " ", " ", "X"] # left diagonal X
+
+#out = won?(board)
+#puts out
