5efd1acca25c450ad3542919e4eb966e8baf4aa9
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-26 21:45:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,61 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+#board = [ "0 ", "1 ", "2 ",
+          #"3 ", "4 ", "5 ", 
+          #"6 ", "7", "8"]
+
+WIN_COMBINATIONS=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]]
+                 
+def won(board)
+  #chech if all position are taken
+      # check win combinations
+  # board.each do |position|
+    # position_taken?(board, )
+end
+
+def full(board)
+    all_busy = board.all? do |element|
+      element=="X"|| element =="O" 
+    end
+    return all_busy
+end
+
+def draw()
+end
+
+def over()
+end
+
+def winner()
+end 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
