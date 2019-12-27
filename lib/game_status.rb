dc00c8e56a3c04fc26aed0dcd6edbe922123c61b
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-27 06:05:50 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,18 +18,38 @@ WIN_COMBINATIONS=[
   [1,4,7],
   [2,5,8]]
                  
-def won(board)
-  #chech if all position are taken
-      # check win combinations
-  # board.each do |position|
-    # position_taken?(board, )
+def won?(board)
+  full?(board)
+    # check win combinations
+      WIN_COMBINATIONS.each do |combination| 
+        win_index_1 = combination[0]
+        win_index_2 = combination[1]
+        win_index_3 = combination[2]
+        position_1 = board[win_index_1] 
+        position_2 = board[win_index_2] 
+        position_3 = board[win_index_3] 
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+          return combination 
+        # false
+        # else 
+          return false
+        end 
+  end 
 end
 
-def full(board)
-    all_busy = board.all? do |element|
-      element=="X"|| element =="O" 
-    end
-    return all_busy
+def full?(board)
+    if (board.all? do |element|
+      element == " " || element == nil || element==""
+      end)
+      # puts "borad empty"
+      return false
+    elsif (board.any?{|element| element ==" " || element== nil})#=> true
+      puts "borad in progress"
+      false 
+    else
+      puts "borad full"
+      true
+    end 
 end
 
 def draw()
